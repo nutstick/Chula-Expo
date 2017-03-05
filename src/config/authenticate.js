4fb36afb1843ebb9c08f1e36ffc57f3eb71f2516
nutstick <nuttapat.kirawittaya@gmail.com>
2017-03-05 18:14:29 +0700

    Add method deserializeToken

--- src/config/authenticate.js
+++ src/config/authenticate.js
@@ -1,5 +1,6 @@
 // let passport = null;
 const passport = require('passport');
+const jwt = require('json-web-token');
 
 module.exports = {
   isAuthenticated: (req, res, next) => {
@@ -27,20 +28,19 @@ module.exports = {
   },
 
   deserializeToken: (req, res, next) => {
-    passport.authenticate('jwt', (err, user) => {
-      if (err) {
-        return res.sendError(5, err);
-      }
-      if (!user) {
-        return next();
-      }
-      req.logIn(user, { session: false }, (err) => {
+    const extractHeader = req.headers.Authorization.split(' ');
+    if (extractHeader[0] === 'JWT') {
+      jwt.decode(process.env.JWT_SECRET, extractHeader[1], (err, decodedPayload, decodedHeader) => {
         if (err) {
-          return next(err);
+          next(err);
+        } else {
+          req.user = decodedPayload.sub;
+          next();
         }
-        return next();
-      });
-    })(req, res, next);
+      })
+    } else {
+      next();
+    }
   },
 
   isAdmin: (req, res, next) => {
