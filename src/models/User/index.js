25062e6abb910cd79bf6b237a799bb4cf94143ef
Kitipong <kittipong1996@hotmail.com>
2017-03-15 05:00:54 +0700

    fixed-apil

--- src/models/User/index.js
+++ src/models/User/index.js
@@ -147,18 +147,6 @@ const UserSchema = new mongoose.Schema({
    });
  });
 
- UserSchema.pre('update', function update(next) {
-   const user = this;
-   bcrypt.genSalt(10, (err, salt) => {
-     if (err) { return next(err); }
-     bcrypt.hash(user.password, salt, null, (err, hash) => {
-       if (err) { return next(err); }
-       user.password = hash;
-       next();
-     });
-   });
- });
-
 /**
  * Helper method for validating user's password.
  */
