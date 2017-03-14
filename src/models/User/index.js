af3f1ec8c78d267643e909c3a95955520c3ad636
Kitipong <kittipong1996@hotmail.com>
2017-03-15 04:03:45 +0700

    a

--- src/models/User/index.js
+++ src/models/User/index.js
@@ -141,6 +141,7 @@ const UserSchema = new mongoose.Schema({
      if (err) { return next(err); }
      bcrypt.hash(user.password, salt, null, (err, hash) => {
        if (err) { return next(err); }
+       console.log(hash);
        user.password = hash;
        next();
      });
