c7762e0d029c4b0f7d6bcd760412b7769abbf553
Kitipong <kittipong1996@hotmail.com>
2017-03-16 01:40:01 +0700

    fixed request

--- src/routes/api/me/index.js
+++ src/routes/api/me/index.js
@@ -47,9 +47,17 @@ router.get('/', isAuthenticatedByToken, (req, res) => {
 
 
 router.get('/where', deserializeToken, (req, res) => {
+  const qs = {};
+  qs.lat = req.query.latitude;
+  qs.lng = req.query.longitude;
+  if (req.user) {
+    qs.u = req.user;
+  }
+
   request.get({
-    uri: 'http://104.199.143.190/area?lat=' + req.query.latitude + '&lng=' + req.query.longitude + (req.user?('&u=' + req.user):''),
-    timeout: 1200
+    uri: 'http://104.199.143.190/area',
+    qs,
+    timeout: 1000
   },
   (err, r, ans) => {
     if (err) {
